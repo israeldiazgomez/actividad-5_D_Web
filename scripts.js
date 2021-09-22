2b93bdec783c565712764f9f55104e6ca506f85b
israel25D <ijesusdiaz@uniguajira.edu.co>
2021-09-22 09:15:33 -0500

    israel

---
+++ scripts.js
@@ -0,0 +1,20 @@
+let products = [];
+let total = 0;
+
+function add(product, price){
+    console.log(product, price);
+    products.push(product);
+    total = total + price;
+    document.getElementById("check").innerHTML = `Pagar $${total}`
+
+}
+
+
+
+
+
+
+function pay(){
+console.log(product,price);
+window.alert(products.join(", \n"));
+}
